8ef090671abfc7e0642c6eca240238774c2a8b80
duynk <duynkhe170075@fpt.edu.vn>
2024-11-26 20:07:29 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,17 @@
 import "./style.css";
 const MyComponent = () => {
+  // const hoidanit = "duy";
+  //const hoidanit = null; // kieu boolean, undefine,null khong in ra gia tri gi ca
+  // const hoidanit = [1, 2, 3];
+  const hoidanit = {
+    name: "duy",
+    age: 25,
+  };
+
   return (
     <>
-      <div>eric & hoidanit update</div>
+      <div>{JSON.stringify(hoidanit)} & hoidanit update</div>
+      <div>{console.log("ERIC")}</div>
       <div className="child" style={{ borderRadius: "10px" }}>
         child
       </div>
