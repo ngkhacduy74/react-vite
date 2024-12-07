4f5581ec31e827d6502d633055df1445e2a4ed83
duynk <duynkhe170075@fpt.edu.vn>
2024-12-07 22:35:43 +0700

    video props

---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,12 @@
+const TodoData = ({ name }) => {
+  // console.log("kajhsdkahsd", props);
+  // const { name, age } = props;
+  return (
+    <div className="todo-data">
+      <div>My name is {name}</div>
+      <div>Learning React</div>
+      <div>Learning Python</div>
+    </div>
+  );
+};
+export default TodoData;
---
+++ src/components/todo/TodoNew.jsx
@@ -0,0 +1,9 @@
+const TodoNew = () => {
+  return (
+    <div className="input-add">
+      <input type="text" className="input" />
+      <button className="button">Add</button>
+    </div>
+  );
+};
+export default TodoNew;
