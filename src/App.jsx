d0a764824c7f3af777a89beee1f5b95ce610e04c
ngkhacduy74 <duycaduoc0704@gmail.com>
2025-02-05 09:49:43 +0700

    vide0 41

--- src/App.jsx
+++ src/App.jsx
@@ -29,11 +29,21 @@ const App = () => {
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew addNewTodo={addNewTodo} />
-      <TodoData todoList={todoList} />
 
-      <div className="todo-image">
-        <img src={reactLogo} />
-      </div>
+      {todoList.length === 0 ? (
+        <div className="todo-image">
+          <img src={reactLogo} />
+        </div>
+      ) : (
+        <TodoData todoList={todoList} />
+      )}
+      {/* {todoList.length !== 0 && <TodoData todoList={todoList} />}
+
+      {todoList.length === 0 && (
+        <div className="todo-image">
+          <img src={reactLogo} />
+        </div>
+      )} */}
     </div>
   );
 };
