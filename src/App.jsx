ae93e3bee5e27f9ca71f2f162993eea54e1b6033
duynk <duynkhe170075@fpt.edu.vn>
2025-03-31 23:52:14 +0700

    video 48

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import TodoData from "./components/todo/TodoData";
 import TodoNew from "./components/todo/TodoNew";
 import reactLogo from "./assets/react.svg";
 import { useState } from "react";
+import Header from "./components/layout/header";
+import Footer from "./components/layout/footer";
+import { Outlet } from "react-router-dom";
 const App = () => {
   const [todoList, setTodoList] = useState([
     // { id: 1, name: "Learning React" },
@@ -30,25 +33,30 @@ const App = () => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
   return (
-    <div className="todo-container">
-      <div className="todo-title">Todo List</div>
-      <TodoNew addNewTodo={addNewTodo} />
+    <>
+      <Header />
+      <div className="todo-container">
+        <div className="todo-title">Todo List</div>
+        <TodoNew addNewTodo={addNewTodo} />
 
-      {todoList.length === 0 ? (
-        <div className="todo-image">
-          <img src={reactLogo} />
-        </div>
-      ) : (
-        <TodoData todoList={todoList} deleteTodo={deleteTodo} />
-      )}
-      {/* {todoList.length !== 0 && <TodoData todoList={todoList} />}
+        {todoList.length === 0 ? (
+          <div className="todo-image">
+            <img src={reactLogo} />
+          </div>
+        ) : (
+          <TodoData todoList={todoList} deleteTodo={deleteTodo} />
+        )}
+        {/* {todoList.length !== 0 && <TodoData todoList={todoList} />}
 
       {todoList.length === 0 && (
         <div className="todo-image">
           <img src={reactLogo} />
         </div>
       )} */}
-    </div>
+      </div>
+      <Outlet />
+      <Footer />
+    </>
   );
 };
 
