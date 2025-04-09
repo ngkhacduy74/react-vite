afc6bb36850da3bb94cd9fde8587b66d2c5ddf84
duynk <duynkhe170075@fpt.edu.vn>
2025-04-09 16:03:26 +0700

    video 67

--- src/pages/users.jsx
+++ src/pages/users.jsx
@@ -1,4 +1,15 @@
+import UserForm from "../components/user/user.form";
+import UserTable from "../components/user/user.table";
+
 const UsersPage = () => {
-  return <div>Users Page</div>;
+  return (
+    <div>
+      Users Page
+      <div>
+        <UserForm />
+        <UserTable />
+      </div>
+    </div>
+  );
 };
 export default UsersPage;
